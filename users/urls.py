3474d65d0e4f0010c1164d5e409e58c406de6d66
Ankita Jha <ankita6mar@gmail.com>
2026-02-10 00:59:23 +0530

    Added urls, views files

---
+++ users/urls.py
@@ -0,0 +1,17 @@
+"""
+URL mappings for the user API.
+"""
+from django.urls import path
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from users import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('me/', views.ManageUserView.as_view(), name='me'),
+    path('token/', TokenObtainPairView.as_view(), name='token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+"""
+Views for the user API.
+"""
+from rest_framework import generics, authentication, permissions
 
-# Create your views here.
+from users.serializers import UserSerializer
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system."""
+    serializer_class = UserSerializer
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
